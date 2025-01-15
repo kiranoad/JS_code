b64403eac8bb10d8aa3396497a45f85826cf7d6f
Kiran Oad <150772533+kiranoad@users.noreply.github.com>
2025-01-15 17:54:03 +0000

    second commit

---
+++ 01_basic/datatype.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer version
+
+// alert("hello");  we are using nodejs, not browser
+
+// -------primitive--------
+// String => ""
+// Number => 087854
+// bigInt => ""
+// boolean => true/false
+// null => standalone value
+// undefined = not assing value
+// symbol = unique
+// -------non primitive------
+// object
+// array
+
+let king = "kiran";
+
+console.log(king);
+
---
+++ 01_basic/variable.js
@@ -0,0 +1,18 @@
+const userId = 1234;
+let accountEmail  = "kiran";
+var  accountPassword = "12334" 
+accountCity = "ktm";
+
+// userId = 2345;
+
+accountEmail = "hari"
+
+accountPassword ="123344"
+accountCity = "pkr"
+
+console.table([userId,accountEmail,accountPassword,accountCity]);
+
+
+/* 
+Prefer not to use var beacuse of issue in block scope and functional scope
+*/
