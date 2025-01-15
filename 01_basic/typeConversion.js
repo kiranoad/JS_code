64c17b2c01a407bb86ac4c72efa63d2d26ad533b
Kiran Oad <150772533+kiranoad@users.noreply.github.com>
2025-01-15 18:17:05 +0000

    type commit

---
+++ 01_basic/typeConversion.js
@@ -0,0 +1,27 @@
+// let score = undefined;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" ==> 33
+// "33sdf" ==> NaN
+// true => 1; false => 0
+
+// let isLoggedIn = 12 ;
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+// console.log(isLoggedIn);
+
+let someNumber = 234
+let stringSomeNumber = String(someNumber);
+console.log(stringSomeNumber);
+console.log(typeof(stringSomeNumber));
+
+
+
+
