f3b92b5c08e7c965af0cc5d2a5a2d66a879e154a
Kiran Oad <150772533+kiranoad@users.noreply.github.com>
2025-01-17 18:41:13 +0000

    date commit

---
+++ 01_basic/date.js
@@ -0,0 +1,42 @@
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toJSON());
+
+
+
+
+// let myCreatedDate = new Date(2024, 0, 23, 5, 3);
+
+// let myCreatedDate = new Date("2024-01-13");
+let myCreatedDate = new Date("01-03-2024");
+
+
+// console.log(myCreatedDate.toLocaleString());
+
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getDay());
+// console.log(newDate.getHours());
+// console.log(newDate.getUTCMonth());
+
+
+
+// new newDate.toLocaleString('default',{
+//     weekday:"long",
+// })
+// console.log(newDate);
+
+
+
+
+
+
