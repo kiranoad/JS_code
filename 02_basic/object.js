a39656625bc35db70241bc61bed5b9ac1907266a
Kiran Oad <150772533+kiranoad@users.noreply.github.com>
2025-01-21 08:55:00 +0000

    commit

--- 02_basic/object.js
+++ 02_basic/object.js
@@ -2,114 +2,114 @@
 
 // object literals
 
-// const mySy =Symbol("key1");
+const mySy =Symbol("key1");
 
-// const jsUser ={ 
-//     name: "kiran", "full name":"kiran oad",
-//     [mySy]:"key1",
-//     age: 18,
-//     location:"nepal",
-//     email:"kian@gmail.com",
-//     isLoggedIn:false,
-//     lastLoginDay:["Monday","saturday"]
-// }
-// // console.log(jsUser.email);
-// // console.log(jsUser["email"]);
-
-// // console.log(jsUser.full name); // it is not used
-// console.log(jsUser["full name"]);
-// console.log(jsUser[mySy]);
-
-// // Object.freeze(jsUser);
-
-
-// jsUser.email = "sanu@gmail.com"
+const jsUser ={ 
+    name: "kiran", "full name":"kiran oad",
+    [mySy]:"key1",
+    age: 18,
+    location:"nepal",
+    email:"kian@gmail.com",
+    isLoggedIn:false,
+    lastLoginDay:["Monday","saturday"]
+}
 // console.log(jsUser.email);
+// console.log(jsUser["email"]);
 
-// jsUser.greeting = function(){
-//     console.log("hello js user");
+// console.log(jsUser.full name); // it is not used
+console.log(jsUser["full name"]);
+console.log(jsUser[mySy]);
+
+// Object.freeze(jsUser);
+
+
+jsUser.email = "sanu@gmail.com"
+console.log(jsUser.email);
+
+jsUser.greeting = function(){
+    console.log("hello js user");
     
-// }
+}
 
 
-// jsUser.greetingTwo = function(){
-//     console.log(`hello js user,${this.name}`);
+jsUser.greetingTwo = function(){
+    console.log(`hello js user,${this.name}`);
     
-// }
+}
 
-// console.log(jsUser.greeting());
-// console.log(jsUser.greetingTwo());
-
-
-
-
-
-
-// singleton
+console.log(jsUser.greeting());
+console.log(jsUser.greetingTwo());
 
 
 
 
 
-// // const tinderUser = new Object().    // singleton objecct 
-// const tinderUser ={}   // non singletone Object 
+
+singleton
 
 
-// tinderUser.id ="123abc"
-// tinderUser.name = "kiran"
-// tinderUser.isLoggedIn = false;
-
-// // console.log(tinderUser);
-
-// const regularUser = {
-//     email: "user@gamil.com",
-//     fullname:{
-//         userfullname:{
-//             firstName:"kiran",
-//             lastName:"oad"
-//         }
-//     }
-// }
-// // console.log(regularUser.fullname.userfullname);
-
-// const obj1 = {1:"a", 2:"b"}
-// const obj2 = {3:"a", 4:"b"}
-// const obj4 = {5:"a", 6:"b"}
 
 
-// // const obj3 = {obj1, obj2}
-// // const obj3 = Object.assign({},obj1,obj2,obj4)
 
-// const obj3 = {...obj1, ...obj2, ...obj4}
+// const tinderUser = new Object().    // singleton objecct 
+const tinderUser ={}   // non singletone Object 
 
-// // console.log(obj3);
 
-// const users = [
-//     {
-//         id:1,
-//         email:"kiran@gmail.com"
-//     },
-//     {
-//         id:1,
-//         email:"kiran@gmail.com"
-//     },
-//     {
-//         id:1,
-//         email:"kiran@gmail.com"
-//     },
-//     {
-//         id:1,
-//         email:"kiran@gmail.com"
-//     }
-// ]
-// users[1].email
+tinderUser.id ="123abc"
+tinderUser.name = "kiran"
+tinderUser.isLoggedIn = false;
+
 // console.log(tinderUser);
 
-// console.log(Object.keys(tinderUser));
-// console.log(Object.values(tinderUser));
-// console.log(Object.entries(tinderUser));
+const regularUser = {
+    email: "user@gamil.com",
+    fullname:{
+        userfullname:{
+            firstName:"kiran",
+            lastName:"oad"
+        }
+    }
+}
+// console.log(regularUser.fullname.userfullname);
 
-// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"a", 4:"b"}
+const obj4 = {5:"a", 6:"b"}
+
+
+// const obj3 = {obj1, obj2}
+// const obj3 = Object.assign({},obj1,obj2,obj4)
+
+const obj3 = {...obj1, ...obj2, ...obj4}
+
+// console.log(obj3);
+
+const users = [
+    {
+        id:1,
+        email:"kiran@gmail.com"
+    },
+    {
+        id:1,
+        email:"kiran@gmail.com"
+    },
+    {
+        id:1,
+        email:"kiran@gmail.com"
+    },
+    {
+        id:1,
+        email:"kiran@gmail.com"
+    }
+]
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
 
  
