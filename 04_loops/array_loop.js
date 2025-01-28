de19d7ffbdff96e41fed5c33243f869044a0dc71
Kiran Oad <150772533+kiranoad@users.noreply.github.com>
2025-01-28 07:27:23 +0000

    commmit

---
+++ 04_loops/array_loop.js
@@ -0,0 +1,16 @@
+// for of loops
+
+// ["", "", ""]
+// [{},{}, {}]
+
+const arr =[1,2,3,4,5]
+
+for(const num of arr){
+    console.log(num);
+
+}
+const greeting = "Helllo world!";
+
+for (const greet of greeting){
+    console.log(`each char is ${greet}`);
+}
