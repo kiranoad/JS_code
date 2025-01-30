9cac537d6ddc36d9f89ebb4232e7069db7f8c692
Kiran Oad <150772533+kiranoad@users.noreply.github.com>
2025-01-30 17:52:48 +0000

    last commit from safari

---
+++ DOM/js/index.js
@@ -0,0 +1,25 @@
+
+// const parent = document.querySelector('.parent')
+// console.log(parent);
+// console.log(parent.children);
+// console.log(parent.children[1].innerHTML);
+// const parent = document.querySelector('.parent');
+
+// for (let i = 0; i < parent.length; i++) { 
+//     console.log(parent.children[i].innerHTML);
+// }
+
+
+// parent.children[1].style.color = "red";
+// console.log(parent.firstElementChild);
+// console.log(parent.lastElementChild);
+
+
+const dayOne = document.querySelector('.day');
+console.log(dayOne);
+console.log(dayOne.parentElement);
+// console.log(dayOne.nextElementSibling);
+
+
+
+
